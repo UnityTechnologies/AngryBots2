ee1d1235d4984a7e90b115c61c9613aa46ab0a9a
Andy Touch <andyt@unity3d.com>
2018-12-10 17:56:02 +0100

    Added Debug Canvas to Toggle Settings on Device
    Current Options: FPS Display, PPS, High/Low LWRP Asset

---
+++ AngryBots2_Project/Assets/Scripts/Utilities/DebugCanvasManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering.LightweightPipeline;
+
+public class DebugCanvasManager : MonoBehaviour
+{
+    [Header("References")]
+    public Canvas debugWindow;
+    public GameObject fpsDisplay;
+    public GameObject postProcessingVolumes;
+    public GameObject LWRPLowDisplay;
+    public GameObject LWRPHighDisplay;
+
+    [Header("LWRP Assets")]
+    public LightweightRenderPipelineAsset LWRPLowQualityAsset;
+    public LightweightRenderPipelineAsset LWRPHighQualityAsset;
+
+    void Start()
+    {
+        HideUI();
+        
+    }
+
+    void HideUI()
+    {
+        debugWindow.enabled = false;
+        fpsDisplay.SetActive(false);
+        LWRPLowDisplay.SetActive(false);
+    }
+
+    public void ToggleDebugWindow(bool newState)
+    {
+        debugWindow.enabled = newState;
+    }
+
+    public void ToggleFPSDisplay(bool newState)
+    {
+        fpsDisplay.SetActive(newState);
+    }
+
+    public void TogglePostProcessing(bool newState)
+    {
+        postProcessingVolumes.SetActive(newState);
+    }
+
+    public void SwitchLWRPAsset(int newAssetID)
+    {
+
+        //0 = Low
+        //1 = High
+
+        switch(newAssetID)
+        {
+            case 0:
+                GraphicsSettings.renderPipelineAsset = LWRPLowQualityAsset;
+                LWRPLowDisplay.SetActive(true);
+                LWRPHighDisplay.SetActive(false);
+                break;
+
+            case 1:
+                GraphicsSettings.renderPipelineAsset = LWRPHighQualityAsset;
+                LWRPLowDisplay.SetActive(false);
+                LWRPHighDisplay.SetActive(true);
+                break;
+        }
+    }
+
+
+    
+}
