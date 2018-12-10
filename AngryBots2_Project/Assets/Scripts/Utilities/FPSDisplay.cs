9cd41d0fe27b8e2777c7eff8e271402eedd23650
Andy Touch <andyt@unity3d.com>
2018-12-10 12:30:45 +0100

    Updated LWRP to 4.2.0
    - Tweaked LWRP Asset Settings
    - Added FPS Display (For Mobile Testing)

---
+++ AngryBots2_Project/Assets/Scripts/Utilities/FPSDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public Text fpsText;
+
+	float deltaTime;
+	
+	void Update ()
+	{
+		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        SetFPS();
+	}
+
+	void SetFPS()
+	{
+		float msec = deltaTime * 1000.0f;
+		float fps = 1.0f / deltaTime;
+		fpsText.text = string.Format("FPS: {0:00.} ({1:00.0} ms)", fps, msec);
+	}
+}
---
+++ AngryBots2_Project/Assets/Scripts/Utilities/ToggleCanvas.cs
@@ -0,0 +1,52 @@
+﻿/* To Toggle on and off the FPS Canvas
+
+Editor - Click anywhere in the Game View
+On a touch-screen Device - Hold one finger on the screen and tap with a second finger
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleCanvas : MonoBehaviour
+{
+    public Canvas canvas;
+    private bool canvasVisible = false;
+
+
+    void Start()
+    {
+        canvas.enabled = canvasVisible;
+    }
+
+    void Update()
+    {
+       int fingerCount = 0;
+
+       foreach(Touch touch in Input.touches)
+       {
+           if(touch.phase == TouchPhase.Began)
+           {
+               fingerCount++;
+           }
+       }
+
+       if(fingerCount > 0)
+       {
+           ToggleCanvasVisibility();
+       }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            ToggleCanvasVisibility();
+        }
+    }
+
+    void ToggleCanvasVisibility()
+    {
+        canvasVisible = !canvasVisible;
+        canvas.enabled = canvasVisible;
+    }
+}
